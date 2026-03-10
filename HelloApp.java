6ec64d6c3def65a8a215c61976d64d50d545b17f
Tarun Suresh <67192301+TarunSuresh07@users.noreply.github.com>
2026-03-10 13:37:38 +0530

    Update HelloApp.java file

--- HelloApp.java
+++ HelloApp.java
@@ -24,6 +24,6 @@ public class HelloApp {
   public static void main(String[] args) {
     // Get the name from command-line argument
     String name = args[0];
-    System.out.println("Hello, + name + "!");
+    System.out.println("Hello, " + name + "!");
   }
 }
