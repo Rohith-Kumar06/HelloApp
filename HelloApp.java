8471bb817fe9867d5b3569212a1ed514a0165106
Tarun Suresh <67192301+TarunSuresh07@users.noreply.github.com>
2026-03-26 13:50:29 +0530

    Update HelloApp.java

--- HelloApp.java
+++ HelloApp.java
@@ -12,7 +12,7 @@
 If names are provided, it will display "Hello, [Name1], [Name2], ...!" to the
 * console.
 If no names are provided, it will display "Hello, World!"
-* @author Deve
+* @author Tarun S
 * @version 7.0
 * @since UC1
 */
